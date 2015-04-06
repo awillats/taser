f4763005ec6607964065fba69c9edc8444e5a22c
awillats <awillats3@gatech.edu>
2015-04-06 12:16:30 -0400

    stim post-processing started
    stimCompile, stimOrder, stimCheck

--- eg/superSlim/stimCompile.m
+++ eg/superSlim/stimCompile.m
@@ -1,3 +1,10 @@
+%% TO DO
+%{
+-improve ordering (display how many left?
+-display compiled results
+-turn these into functions??
+%}
+
 clear
 
 D = dir;
@@ -125,41 +132,3 @@ for row = 1:nrows
 end
 fclose all;
 
-%%
-fprintf('total file length = %d\n',sum(sum(Ls)))
-allSuperDirNames
-asdn = allSuperDirNames
-
-i=0;
-fprintf('\n')
-fprintf('\n')
-fprintf('\n')
-for fn = allSuperDirNames
-    fprintf('%i: %s  ',i,fn{1})
-    i = i+1;
-    if (mod(i,6) == 0)
-        fprintf('\n')
-    end
-end
-fprintf('\n')
-nI = 0;
-order = [];
-
-while (nI < sdN)
-    o = input('next stim: ');
-    order = [order,o];
-    nI = length(order);
-    
-end
-%%
-fprintf('\n')
-revOrder = fliplr(order)
-for o = revOrder
-    fprintf('%s,',allSuperDirNames{o+1})
-    
-end
-fprintf('\n')
-for o = revOrder
-    fprintf('%i,',o)
-end
-fprintf('\n')
